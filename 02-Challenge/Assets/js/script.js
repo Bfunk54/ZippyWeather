2842566727851b1774650a1f4963b9ec552a0bdb
Bfunk54 <ben_fein@icloud.com>
2022-11-01 22:17:32 -0400

    cleaned up old code and cleared all console logs from the JS file

--- 02-Challenge/Assets/js/script.js
+++ 02-Challenge/Assets/js/script.js
@@ -1,8 +1,5 @@
-
 // Global variables
 var city = '';
-
-
 
 // DOM element references
 const searchForm = document.getElementById('searchForm');
@@ -12,12 +9,6 @@ const fiveDayForecast = document.getElementById('5dayForecast');
 const searchHistory = document.getElementById('searchHistory');
 const searchButton = document.getElementById('searchButton');
 const cityName = document.getElementById('theNewCity');
-// search form
-// search input
-// container/section for today's weather
-// container/section for the forecast 
-// search history container
-
 
 // Function to display the search history list.
 function renderSearchHistory(search) {
@@ -32,9 +23,7 @@ function renderSearchHistory(search) {
       // append to the search history container
       searchHistory.appendChild(historyButton);
     }
-      
   }
-  
     let searchArr = [];
 
   // Function to update history in local storage then updates displayed history.
@@ -55,7 +44,6 @@ function renderSearchHistory(search) {
      console.log(historyStr);
      searchArr.push(historyStr);
      console.log(searchArr);
-  
     // set search history array equal to what you got from local storage
     renderSearchHistory();
   }
@@ -64,12 +52,10 @@ function renderSearchHistory(search) {
   function renderCurrentWeather(city, weather) {
     // Store response data from our fetch request in variables
       // temperature, wind speed, etc.
-      console.log(weather);
-
       let temp = weather.main.temp;
       let windSpeed = weather.wind.speed;
       let humidity = weather.main.humidity;
-      console.log(temp, windSpeed, humidity);
+
       var date = new Date(weather.dt_txt);
 
       var day = date.getDate();
@@ -84,9 +70,7 @@ function renderSearchHistory(search) {
 
       let weatherImg = document.createElement('img');
       weatherImg.src = imgUrl;
-      console.log(weatherImg);
-  
-  
+
     // document.create the elements you'll want to put this information in  
       let dataUl = document.createElement('ul');
   
@@ -96,11 +80,8 @@ function renderSearchHistory(search) {
       dataUl.innerHTML += '<li> Humidity: ' + humidity + '%</li>';
   
     // give them their appropriate content
-      console.log(todaysWeather);
       todaysWeather.appendChild(dataUl);
-      // let theCity = JSON.stringify(city);
-      console.log(city);
-      // city.innerHTML;
+
       //Clear the last city and create a text node with the city name and put that on the page
       cityName.innerHTML = '';
       
@@ -109,10 +90,9 @@ function renderSearchHistory(search) {
       cityName.appendChild(weatherImg)
 
       todaysWeather.classList.remove('hide');
-
   }
   
-  // Function to display a FORECAST card given an object (from our renderForecast function) from open weather api
+  // Function to display a FORECAST card given an object (from the renderForecast function) from open weather api
   // daily forecast.
   function renderForecastCard(forecast) {
 
@@ -128,22 +108,17 @@ function renderSearchHistory(search) {
       let imgArr = [];
 
       for (var i = 0; i < 5; i++){
-      
-      console.log(forecast);
+
       let temp = forecast[i].main.temp;
       tempArr.push(((temp - 273.15) * 1.8 + 32).toFixed(2));
-      console.log(tempArr);
       
       let windSpeed = forecast[i].wind.speed;
-      console.log(windSpeedArr);
       windSpeedArr.push(JSON.stringify(windSpeed));
      
       let humidity = forecast[i].main.humidity
       humidityArr.push(JSON.stringify(humidity));
-      console.log(humidityArr);
 
       let imgId = forecast[i].weather[0].icon;
-      console.log(imgId);
 
       let date = new Date(forecast[i].dt_txt);
 
@@ -158,11 +133,8 @@ function renderSearchHistory(search) {
 
       let weatherImg = document.createElement('img');
       weatherImg.src = imgUrl;
-      console.log(JSON.stringify(weatherImg));
       imgArr.push(weatherImg)
-      console.log(imgArr);
-      // console.log(humidity);
-      }
+  }
     // Create elements for a card
     
     for (var i = 0; i < 5; i++){
@@ -203,20 +175,15 @@ function renderSearchHistory(search) {
   let fiveDayArr = [];
   // Get data for 5 day forecast, turn each day into strings and put in the array
   for (var i = 0; i <= 39; i = i + 8) {
-    console.log(dailyForecast[i]);
     fiveDayArr.push(dailyForecast[i]);
-    console.log(fiveDayArr);
   }
-  
       // send the data to our renderForecast function an array
           renderForecastCard(fiveDayArr);
     }
   
-
 // Passes in the city name and data objects and calls them to other functions
   function renderItems(city, data) {
     renderCurrentWeather(city, data.list[0]);
-    console.log(city, data.list);
     renderForecast(data.list);
   }
   
@@ -227,16 +194,13 @@ function renderSearchHistory(search) {
     let long = location.coord.lon;
     let lat = location.coord.lat;
     let name = location.name;
-    console.log('longitude is: ' + long);
-    console.log('latitude is: ' + lat);
-    console.log('name is: ' + name);
+
     // api url
     let apiKey = 'cc89612e36b533aff0217169aedace13';
     let apiURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&appid=' + apiKey;
     // fetch, using the api url, .then that returns the response as json, .then that calls renderItems(city, data)
         const response = await fetch(apiURL);
         const data = await response.json();
-        console.log(name, data);
         renderItems(name, data);
   }
   
@@ -245,14 +209,12 @@ function renderSearchHistory(search) {
     const weathUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + search + '&appid=cc89612e36b533aff0217169aedace13'
         const response = await fetch(weathUrl);
         const data = await response.json();
-        console.log(data);
     // fetch with your url, .then that returns the response in json, .then that does 2 things - calls appendToHistory(search), calls fetchWeather(the data)
         appendToHistory(search)
         fetchWeather(data)
   }
   
   function handleSearchFormSubmit(searchInput) {
-    console.log(searchInput);
     // Don't continue if there is nothing in the search form
     if (!searchInput.value) {
       return;
@@ -262,7 +224,6 @@ function renderSearchHistory(search) {
     var search = searchInput.value.trim();
     fetchCoords(search);
     searchInput.value = '';
-    console.log(search);
   }
   
   function handleSearchHistoryClick(e) {
